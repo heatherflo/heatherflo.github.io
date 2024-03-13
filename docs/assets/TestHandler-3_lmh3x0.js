b24f43233f0533e5bd4f40e9d91faff5be0ea51d
Heather Floyd <heatherflo@gmail.com>
2024-03-13 16:05:21 -0600

    change of design

--- docs/assets/TestHandler-3_lmh3x0.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-h9T1bBEL.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="font">
     <p class="m-3 text-center ">About Me</p>
-    <p class="fs-2">
+    <p class="fs-2 m-2">
 
       Mom of 5, previous photographer and CEO of Floyd Family Photography, avid outdoor adventurer, Heather is a newly
-      reinvented software developer. She is excited to work front end applications in order to help companies sell their
+      reinvented software developer. She is excited to work front end applications in order to help businesses sell
+      their
       products effectively and efficiently. Making beautiful designs functional to move businesses toward their goals,
-      is
-      her desire and ambition.
+      is her desire and ambition.
     </p>
-    <div class="btn color">
+    <div class="btn btn-success">
       <button class="m-3" @click="sendEmail()">
         Contact Heather
       </button>
