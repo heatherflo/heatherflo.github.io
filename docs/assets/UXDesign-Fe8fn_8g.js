a694f87f9459ecc6ab3dda34933ae8fd7d101b8b
Heather Floyd <heatherflo@gmail.com>
2024-04-24 11:32:08 -0600

    changes to navbar

--- docs/assets/UXDesign-Fe8fn_8g.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t,a as o}from"./index-Ew17zCcG.js";const n={setup(){return{}}},c={class:"component"},a=o("div",null," this is the UX Design page ",-1),r=[a];function _(i,p,d,l,u,f){return s(),t("div",c,r)}const m=e(n,[["render",_]]);export{m as default};
