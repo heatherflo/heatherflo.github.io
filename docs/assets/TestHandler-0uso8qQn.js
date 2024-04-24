894a638c9634087ee58529cefd4c58929bc37459
Heather Floyd <heatherflo@gmail.com>
2024-04-24 11:53:12 -0600

    why is this doing this?

--- docs/assets/TestHandler-0uso8qQn.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-2WOv2wJj.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
