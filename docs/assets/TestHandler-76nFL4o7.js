34d0d6b202a382478f8a20360eb597c6b289a1a1
Heather Floyd <heatherflo@gmail.com>
2024-04-24 11:40:41 -0600

    committing changes

---
+++ docs/assets/TestHandler-76nFL4o7.js
@@ -0,0 +1,1 @@
+import{S as s}from"./index-m-tSfVty.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
