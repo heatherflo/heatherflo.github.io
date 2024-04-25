12b3c431e5a626d897328c1112bc2ff70aa88098
Heather Floyd <heatherflo@gmail.com>
2024-04-25 10:50:03 -0600

    changing home page

---
+++ docs/assets/Photos-AVTZuCyr.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as t,a as e}from"./index-XIttUsN6.js";const c={setup(){return{}}},a={class:"component"},n=e("div",{class:"col-12"},"this is the photos page",-1),r=[n];function _(p,i,d,h,l,f){return o(),t("div",a,r)}const m=s(c,[["render",_]]);export{m as default};
