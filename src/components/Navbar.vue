593add153513046a94997731fb038483d668b6a0
Heather Floyd <heatherflo@gmail.com>
2024-04-29 08:35:44 -0600

    changing navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="container-fluid coll-12 font">
+    <img class="img-fluid" src="/src/assets/img/HJ.jpg" alt="HJ logo ">
+    About
+  </nav>
+</template>
+
+<script>
+
+export default {
+  setup() {
+
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.font {
+  font-family: "Shadows Into Light", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+
+@media screen and (min-width: 576px) {
+  nav {
+    height: 64px;
+  }
+}
+</style>
