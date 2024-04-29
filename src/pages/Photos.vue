964485de2b2d84a1d89d676604718ee9b22f357e
Heather Floyd <heatherflo@gmail.com>
2024-04-29 09:39:00 -0600

    navbar changes

---
+++ src/pages/Photos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        this is the photos page
+        <img src="/src/assets/img/backpack.jpg" alt="">
+        <img src="/src/assets/img/boardwalk.jpg" alt="">
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, ref, onMounted } from 'vue';
+export default {
+  setup() {
+    return {}
+  }
+};
+</script>
+
+
+<style lang="scss" scoped></style>
