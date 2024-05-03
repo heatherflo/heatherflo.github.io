12163e148b5a38ffeb7e3b2caa4fd39cdbbcd7ba
Heather Floyd <heatherflo@gmail.com>
2024-05-03 10:49:56 -0600

    changing image

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid">
+
+    <section class="row">
+      <Title />
+    </section>
+    <section class="row">
+      <Portfolio />
+    </section>
+
+  </div>
+</template>
+
+<script>
+import Portfolio from '../components/Portfolio.vue';
+import Title from '../components/Title.vue';
+import AboutMe from '../components/AboutMe.vue';
+
+
+export default {
+  setup() {
+    return {};
+  },
+  components: { Portfolio, Title, AboutMe }
+}
+</script>
+
+<style scoped lang="scss"></style>
