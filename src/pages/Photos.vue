a03b2b23dbf8106ae4b09a61192437b139ead6e3
Heather Floyd <heatherflo@gmail.com>
2024-05-03 11:15:27 -0600

    image play

---
+++ src/pages/Photos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        This page will house my portfolio of images. Check back later for updates.
+        <img class="img" src="/src/assets/img/backpack.jpg" alt="">
+        <img class="img" src="/src/assets/img/boardwalk.jpg" alt="">
+        <img class="img" src="/src/assets/img/brian.jpg" alt="">
+        <img class="img" src="/src/assets/img/crators.jpg" alt="">
+        <img class="img" src="/src/assets/img/hiddensprings.jpg" alt="">
+        <img class="img" src="/src/assets/img/camp.jpg" alt="">
+        <img class="img" src="/src/assets/img/ladies.jpg" alt="">
+        <img class="img" src="/src/assets/img/kenz.jpg" alt="">
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, ref, onMounted } from 'vue';
+export default {
+  setup() {
+    return {}
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.img {
+  object-fit: center;
+  width: 400px;
+  height: 300px;
+  margin: 0%;
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+}
+</style>
