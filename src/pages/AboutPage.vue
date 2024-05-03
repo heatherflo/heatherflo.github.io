ea720c0e3bca1ef28d1e2d7bef35fcdcd0aaf84b
Heather Floyd <heatherflo@gmail.com>
2024-05-03 11:09:36 -0600

    portfolio images

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <img class="m-3 me" src="/src/assets/img/me2.jpeg" alt="">
+
+    </div>
+    <div class="m-3 fs-4">
+
+      Mom of 5, previous photographer and CEO of Floyd Family Photography, avid outdoor adventurer, Heather is a newly
+      reinvented software developer. She is excited to work front end applications in order to help businesses sell
+      their products effectively and efficiently. Making beautiful designs functional to move businesses toward their
+      goals,is
+      her desire and ambition.
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.me {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  object-fit: cover;
+  border-radius: 50%;
+  width: 300px;
+  height: 300px;
+}
+</style>
