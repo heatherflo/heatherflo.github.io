775dbd155d3411c7c8b0076aa1891362ed47d557
Heather Floyd <heatherflo@gmail.com>
2024-05-06 10:47:46 -0600

    trying hover text

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -12,15 +12,20 @@
 
   <div class="row">
     <div class="col-12 col-md-4 ">
-      <a href="https://keepr.heathermaejohnson.com">
-        <img class="img-fluid hover" src="/img/keepr.png" alt="">
-
-      </a>
+      <div class="image-container" @mouseover="showText('keepr')" @mouseleave="hideText('keepr')">
+        <a href="https://keepr.heathermaejohnson.com">
+          <img class="img-fluid hover" src="/img/keepr.png" alt="">
+          <div class="hover-text" v-show="showKeepr">Keepr</div>
+        </a>
+      </div>
     </div>
     <div class="col-12 col-md-4">
-      <a href="https://tower.heathermaejohnson.com">
-        <img class="img-fluid hover" src="/img/tower.png" alt="">
-      </a>
+      <div class="image-container" @mouseover="showText('tower')" @mouseleave="hideText('tower')">
+        <a href="https://tower.heathermaejohnson.com">
+          <img class="img-fluid hover" src="/img/tower.png" alt="">
+          <div class="hover-text" v-show="showTower">Tower</div>
+        </a>
+      </div>
     </div>
     <div class="col-12 col-md-4">
       <img class="img-fluid hover" src="/img/potholes.jpg" alt="">
@@ -35,7 +40,18 @@
 <script>
 export default {
   setup() {
-    return {}
+    return {
+      showKeepr: false,
+      showTower: false,
+    }
+  },
+  methods: {
+    showText(image) {
+      this[`show${image.charAt(0).toUpperCase() + image.slice(1)}`] = true;
+    },
+    hideText(image) {
+      this[`show${image.charAt(0).toUpperCase() + image.slice(1)}`] = false;
+    }
   }
 };
 </script>
@@ -48,21 +64,36 @@ export default {
 }
 
 img {
-  opacity: 0.5;
-}
-
-img:hover {
   opacity: 1.0;
 }
 
-// :hover {
-//   color:
-// }
+img:hover {
+  opacity: 0.5;
+}
 
 .font {
   font-family: "Shadows Into Light", cursive;
   font-weight: 400;
   font-style: normal;
   font-size: 50px;
+}
+
+.image-container {
+  position: relative;
+}
+
+.hover-text {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 5px;
+  border-radius: 5px;
+  display: none;
+}
+
+.image-container:hover .hover-text {
+  display: block;
 }
 </style>
