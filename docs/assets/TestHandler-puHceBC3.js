e66f7363e47d5076fecf4fa0656b1f99ad503fbf
Heather Floyd <heatherflo@gmail.com>
2024-05-13 11:01:03 -0600

    saving footer changes

---
+++ docs/assets/TestHandler-puHceBC3.js
@@ -0,0 +1,1 @@
+import{S as s}from"./index-jK8VJiO2.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container-fluid">
+
+    <section class="row">
+      <Title />
+    </section>
+    <section class="row">
+      <Portfolio />
+    </section>
+    <section class="row">
+      <Footer />
+    </section>
+
+  </div>
+</template>
+
+<script>
+import Portfolio from '../components/Portfolio.vue';
+import Title from '../components/Title.vue'; import AboutMe from '../components/AboutMe.vue';
+import Footer from '../components/Footer.vue'
+
+
+export default {
+  setup() {
+    return {};
+  },
+  components: { Portfolio, Title, AboutMe, Footer }
+}
+</script>
+
+<style scoped lang="scss"></style>
