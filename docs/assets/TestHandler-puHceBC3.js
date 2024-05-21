07c171fe815d1ed9b4125390bbb572212cfc913e
Heather Floyd <heatherflo@gmail.com>
2024-05-21 11:25:13 -0600

    changes in socials

--- docs/assets/TestHandler-puHceBC3.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-jK8VJiO2.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
