7c12df4b32abb46ee461df372e2f4e24453cf47b
Heather Floyd <heatherflo@gmail.com>
2024-05-21 11:25:20 -0600

    changes in socials

---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="container-fluid">
+    <div class="row">
+
+      <div class="col-10 d-flex m-3 justify-content-center">
+        <a href="https://instagram.com/mmsgoadventure"><i class="mdi mdi-instagram"></i></a>
+
+        <a href="https://github.com/heatherflo"><i class="mdi mdi-github"></i></a>
+
+        <a href="https://www.linkedin.com/in/heathermjohnsonwebdev/"><i class="mdi mdi-linkedin"></i></a>
+      </div>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+
+export default {
+  setup() {
+
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.font {
+  font-family: "Shadows Into Light", cursive;
+  font-weight: 400;
+  font-style: normal;
+  color: black;
+}
+
+.logo {
+  max-width: 40px;
+  max-height: 40px;
+  color: black;
+}
+
+@media screen and (min-width: 576px) {
+  nav {
+    height: 64px;
+  }
+}
+</style>
